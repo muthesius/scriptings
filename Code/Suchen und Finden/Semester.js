e06a44aa4a2a5b145fdf68ef3688128d04a7ed6d
jens-a-e <jens@lea.io>
2012-11-14 14:29:28 +0100

    Pre-Seminar Commit

---
+++ Code/Suchen und Finden/Semester.js
@@ -0,0 +1,65 @@
+// ========================================
+// = Aufgabe: Semesterbelegung als Objekt =
+// ========================================
+
+var mein_semester = {
+  student: {
+    name: "Christoph Christian Eichhorn",
+    studiensemester: 3,
+	matrikelnummer: 901252,
+	studiengang: "Interface Design",
+	studienabschluss: "Master of Arts"
+  },
+  welches: "WS",
+  jahr: "2012/13",
+  veranstaltungen: [ 
+    {
+      titel: "Scriptings",
+      sws: 2,
+      credits: 3,
+      workload: 90,
+      zeit: "14:00-15:30",
+      tag: "Mittwoch"
+    },
+    {
+      titel: "poetische Apparate",
+      sws: 2,
+      credits: 3,
+      workload: 90,
+      zeit: "08:15-09:45",
+      tag: "Freitag"
+    },
+	{
+      titel: "Gebe-Wesen",
+      sws: 2,
+      credits: 15,
+      workload: 450,
+      zeit: "10:00-13:00",
+      tag: "Freitag"
+    },
+	{
+      titel: "Rhino 3D",
+      sws: 2,
+      credits: 2,
+      workload: 90,
+      zeit: "09:00-11:00",
+      tag: "Donnerstag"
+    },
+	{
+      titel: "Read and Write for Design",
+      sws: 2,
+      credits: 3,
+      workload: 90,
+      zeit: "14:00-16:00",
+      tag: "Dienstag"
+    },
+	{
+      titel: "Pre-Thesis Colloquium",
+      sws: 2,
+      credits: 3,
+      workload: 90,
+      zeit: "16:00-18:00",
+      tag: "Dienstg"
+    }
+  ]
+};
---
+++ Code/Suchen und Finden/SuchenUndFinden.js
@@ -0,0 +1,59 @@
+/*
+   Suchen und Finden
+   -----------------
+   Arbeiten mit Objekten, eine Reihe an Aufgaben zum Training
+   von Schleifen und Bedingungen.
+ */
+
+// 1. "Lade" Dein "Semester" so, dass Du es überall über den
+//    Namen _semester_ erreichen kannst.
+
+// console.log(semester); // Teste, ob alles gut ging.
+
+
+
+
+// 1. Drucke jede Veranstaltung einzeln(!) in die Console.
+
+
+
+
+// 2. ... in einem _formatierten_ Text.
+
+
+
+
+// 3. Wie viele Credits hast Du in diesem Semester?
+var gesamt_credits;
+
+
+
+// 4. ... und wie hoch ist Dein Workload?
+var gesamt_workload;
+
+
+
+// 5. Finde die Veranstaltung mit den meisten Credits.
+var joker;
+
+
+
+// 6. Erstelle ein Array mit allen Titeln der Veranstaltungen
+//    Benutze dafür die Push-Funktion eines Arrays.
+var veranstaltungstitel;
+
+
+
+// 7. Generiere den Satz:
+//    "<mein_name> hat in diesem Semester <Seminar1>,<Seminar1>
+//     ... und <letztes_Seminar> belegt."
+var satz;
+
+
+
+// 8. Lade Deine Kommilitonen dazu und erstelle ein Array
+//    mit allen, in diesem Semester belegten Veranstaltungen.
+//    Achte darauf, dass keine doppelt darin vorkommen!
+
+
+
