0ae862545128e35adaf17861f0473a6ebb182cd2
jens-a-e <jens@lea.io>
2012-11-20 18:24:41 +0100

    Fix Lösung 2

--- Code/Suchen und Finden/SuchenUndFinden.js
+++ Code/Suchen und Finden/SuchenUndFinden.js
@@ -24,7 +24,7 @@ for (var i=0; i < semester.veranstaltungen.length; i++) {
 for (var i=0; i < semester.veranstaltungen.length; i++) {
   var v = semester.veranstaltungen[i]
   // "Scriptings", 3 Credits, 2 Semesterwochenstunden mit 90 Stunden Workload im Semester.
-  console.log('"'+v+'", '+v.credits+" Credits, "+v.sws+", Semesterwochenstunden mit "+v.workload+"  Workload im Semester.");
+  console.log('"'+v.titel+'", '+v.credits+" Credits, "+v.sws+", Semesterwochenstunden mit "+v.workload+"  Workload im Semester.");
 }
 
 
