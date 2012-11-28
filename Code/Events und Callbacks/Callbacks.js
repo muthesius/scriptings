bf66994031c0da0ab34365705a0ca3e097c3ee78
jens-a-e <jens@lea.io>
2012-11-28 14:16:11 +0100

    Callbacks 1

---
+++ Code/Events und Callbacks/Callbacks.js
@@ -0,0 +1,15 @@
+// =======================================================
+// = Callback/Handler Situationen am Beispiel mit Arrays =
+// =======================================================
+
+// Achte auf Dein Einrücken! Eine gute Struktur hilft dem Lesen und Verstehen!
+
+
+// 1) .map(function(){...}) als Alternative zu for...
+
+// Als einfaches Bespiel logge alle Veranstaltungen _einzeln_ in die Console
+
+semester.veranstaltungen.map( function(veranstaltung) {
+  console.log(veranstaltung);
+});
+
