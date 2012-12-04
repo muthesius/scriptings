1923195eb873d7eb40d7fae6c58c2693f8cf2e33
jens-a-e <jens@lea.io>
2012-12-04 18:05:39 +0100

    Templates mit Daten

--- Code/Backbone Start/js/backbone-start.js
+++ Code/Backbone Start/js/backbone-start.js
@@ -5,6 +5,9 @@
 var SemesterApp = Backbone.View.extend({
   el: 'body',
   
+  name  : "Semesterplan",
+  about : "Das Semester im Überblick",
+  
   initialize: function(){
     console.log("Eine neue SemesterApp! Das bin ich:", this);
     
@@ -17,8 +20,10 @@ var SemesterApp = Backbone.View.extend({
   
   // Den Content rendern:
   render: function() {
-    // Das Template wird ebenfalls "gerendert" und ...
+    // Das Template wird ebenfalls "gerendert" und einem Daten-Objekt versorgt
+    // Das Daten-Objekt ist hier die View selbst: this
     var viewHtml = this.template(this);
+    
     // heraus kommt HTML, das wir in unser Element einfügen können:
     this.$el.html( viewHtml );
   }
