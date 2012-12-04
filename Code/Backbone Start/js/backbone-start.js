5610a64fb269b145dfd773f4ff82f15c1dfef328
jens-a-e <jens@lea.io>
2012-12-04 16:41:56 +0100

    Backbone Schritt 1 - el legt das Element der View fest

--- Code/Backbone Start/js/backbone-start.js
+++ Code/Backbone Start/js/backbone-start.js
@@ -2,6 +2,11 @@
 // = Backbone Start =
 // ==================
 
-var SemesterApp = Backbone.View.extend({});
+var SemesterApp = Backbone.View.extend({
+  el: 'body'
+});
 
 var app = new SemesterApp();
+
+// Zum Test das el in die Console loggen:
+console.log(app.el);
