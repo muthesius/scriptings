d7aafe2c6baccc2eea17acbf67a45ff97a8d4ef5
jens-a-e <jens@lea.io>
2012-12-04 17:16:09 +0100

    Backbone Schritt 4 - render

--- Code/Backbone Start/js/backbone-start.js
+++ Code/Backbone Start/js/backbone-start.js
@@ -7,7 +7,11 @@ var SemesterApp = Backbone.View.extend({
   
   initialize: function(){
     console.log("Eine neue SemesterApp! Das bin ich:", this);
-    
+  },
+  
+  // Um alle direkten visuellen Aktionen übersichtlich zu halten, sammlt
+  // man diese zentral in der render-Funktion:
+  render: function() {
     // Zum Test, dynamisch Content in die View einzusetzen
     // testen wir mit etwas Text. Backbone bietet einen direkten
     // Zugriff auf das jQuery Element der View durch this.$el
