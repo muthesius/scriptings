5bf645e0ae02472d79ec771111d232e78505bc1a
jens-a-e <jens@lea.io>
2012-12-05 14:17:03 +0100

    Lösung 10 – Testscript aktualisieren

--- Code/Events und Callbacks/script.js
+++ Code/Events und Callbacks/script.js
@@ -2,8 +2,17 @@
 // = Das neue Semester-Objekt in Anwendung =
 // =========================================
 
-var horst = new Student("Horst Hansen",2,"MA","Interfacedesign");
+var horst = new Student("Horst Hansen",2,2410345,"Interfacedesign","MA");
 
 var semester = new Semester(horst,"WS","2012/13");
 
+var v = new Veranstaltung("Scriptings",2,3,"14:30-16:00","Mittwoch");
+
+semester.addVeranstaltung(v);
+
 console.log(semester);
+
+console.log("Habe ich genügen Credits?", semester.creditsCompleted());
+
+console.log("Wieviel Workload habe ich in diesem Semester?", semester.getGesamtWorkload());
+
